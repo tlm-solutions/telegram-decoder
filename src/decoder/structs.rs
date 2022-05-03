c1654c0168c65448a20f1a84c7c8e877d8ee69a8
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2022-05-03 14:25:04 +0200

    update stops.json and use String for BCD parsing

--- src/decoder/structs.rs
+++ src/decoder/structs.rs
@@ -1,7 +1,8 @@
 extern crate derive_builder;
 
-use std::time::{SystemTime, UNIX_EPOCH};
+use hex_string::nibble_to_hexchar;
 use std::fmt;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Telegram {
@@ -9,14 +10,14 @@ pub struct Telegram {
     pub lat: f64,
     pub lon: f64,
     pub station_id: u32,
-    pub line: u32,
-    pub destination_number: u32,
+    pub line: String,
+    pub destination_number: String,
     pub priority: u32,
     pub sign_of_deviation: u32,
     pub value_of_deviation: u32,
     pub reporting_point: u32,
     pub request_for_priority: u32,
-    pub run_number: u32,
+    pub run_number: String,
     pub reserve: u32,
     pub train_length: u32,
     pub junction: u32,
@@ -51,16 +52,34 @@ impl Telegram {
             station_id: station_config.station_id,
             sign_of_deviation: (byte_array[1] >> 7) as u32, //ZV Zeit Vorzeichen
             value_of_deviation: ((byte_array[1] >> 4) & 0x7) as u32, //ZW Zahlen Wert
-            reporting_point: reporting_point,        //MP Melde punkt
+            reporting_point: reporting_point,               //MP Melde punkt
             priority: (byte_array[4] >> 6) as u32,          //PR Prioritet
             request_for_priority: ((byte_array[4] >> 4) & 0x3) as u32, // HA Anforderung Richtung
-            line: 100 * (byte_array[4] & 0xf) as u32 + 10 * (byte_array[5] >> 4) as u32 + (byte_array[5] & 0xf) as u32, // LN Line Nummer
-            run_number: (10 * (byte_array[6] >> 4) + (byte_array[6] & 0xf)) as u32, // KN Kurse Nummer
-            destination_number: (100 * ((byte_array[7] as u32) >> 4)
-                + 10 * ((byte_array[7] as u32) & 0xf)
-                + ((byte_array[8] as u32) >> 4)) as u32, // ZN Zielnummer
-            reserve: ((byte_array[8] >> 3) & 0x1) as u32,                           // R reserve
-            train_length: (byte_array[8] & 0x7) as u32,                             // ZL length
+            line: [
+                nibble_to_hexchar(&(byte_array[4] & 0xf)).unwrap(),
+                nibble_to_hexchar(&(byte_array[5] >> 4)).unwrap(),
+                nibble_to_hexchar(&(byte_array[5] & 0xf)).unwrap(),
+            ]
+            .iter()
+            .cloned()
+            .collect::<String>(), // LN Line Nummer
+            run_number: [
+                nibble_to_hexchar(&(byte_array[6] >> 4)).unwrap(),
+                nibble_to_hexchar(&(byte_array[6] & 0xf)).unwrap(),
+            ]
+            .iter()
+            .cloned()
+            .collect::<String>(), // KN Kurse Nummer
+            destination_number: [
+                nibble_to_hexchar(&(byte_array[7] >> 4)).unwrap(),
+                nibble_to_hexchar(&(byte_array[7] & 0xf)).unwrap(),
+                nibble_to_hexchar(&(byte_array[8] >> 4)).unwrap(),
+            ]
+            .iter()
+            .cloned()
+            .collect::<String>(), // ZN Zielnummer
+            reserve: ((byte_array[8] >> 3) & 0x1) as u32,   // R reserve
+            train_length: (byte_array[8] & 0x7) as u32,     // ZL length
             junction: (reporting_point >> 2) / 10,
             junction_number: (reporting_point >> 2) % 10,
             request_status: reporting_point & 0x3 as u32,
@@ -70,32 +89,52 @@ impl Telegram {
 
 impl PartialEq for Telegram {
     fn eq(&self, other: &Self) -> bool {
-        self.lat == other.lat &&
-            self.lon == other.lon &&
-            self.station_id == other.station_id &&
-            self.sign_of_deviation == other.sign_of_deviation &&
-            self.value_of_deviation == other.value_of_deviation &&
-            self.reporting_point == other.reporting_point &&
-            self.priority == other.priority &&
-            self.request_for_priority == other.request_for_priority &&
-            self.line == other.line &&
-            self.run_number == other.run_number &&
-            self.destination_number == other.destination_number &&
-            self.reserve == other.reserve &&
-            self.train_length == other.train_length &&
-            self.junction == other.junction &&
-            self.junction_number == other.junction_number &&
-            self.request_status == other.request_status
+        self.lat == other.lat
+            && self.lon == other.lon
+            && self.station_id == other.station_id
+            && self.sign_of_deviation == other.sign_of_deviation
+            && self.value_of_deviation == other.value_of_deviation
+            && self.reporting_point == other.reporting_point
+            && self.priority == other.priority
+            && self.request_for_priority == other.request_for_priority
+            && self.line == other.line
+            && self.run_number == other.run_number
+            && self.destination_number == other.destination_number
+            && self.reserve == other.reserve
+            && self.train_length == other.train_length
+            && self.junction == other.junction
+            && self.junction_number == other.junction_number
+            && self.request_status == other.request_status
     }
 }
 
 impl fmt::Display for Telegram {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         const FILE_STR: &'static str = include_str!("../../stops.json");
-        let parsed: serde_json::Map<String, serde_json::Value> = serde_json::from_str(&FILE_STR).expect("JSON was not well-formatted");
+        let parsed: serde_json::Map<String, serde_json::Value> =
+            serde_json::from_str(&FILE_STR).expect("JSON was not well-formatted");
         let junction_string = self.junction.to_string();
-        let junction = parsed.get(&junction_string).map(|u| u.as_str().unwrap()).unwrap_or(&junction_string);
+        let junction = parsed
+            .get(&junction_string)
+            .map(|u| {
+                u.as_object()
+                    .unwrap()
+                    .get("name")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+            })
+            .unwrap_or(&junction_string);
 
-        write!(f, "Line {} Run {} Destination {} - {} / {} / {}", self.line, self.run_number, self.destination_number, junction, self.junction_number, self.request_status)
+        write!(
+            f,
+            "Line {} Run {} Destination {} - {} / {} / {}",
+            self.line,
+            self.run_number,
+            self.destination_number,
+            junction,
+            self.junction_number,
+            self.request_status
+        )
     }
 }
--- src/decoder/test.rs
+++ src/decoder/test.rs
@@ -1,5 +1,5 @@
 //mod decoder;
-use crate::decoder::{Decoder, Config, Telegram};
+use crate::decoder::{Config, Decoder, Telegram};
 
 extern crate derive_builder;
 
@@ -17,13 +17,13 @@ struct TelegramFrame {
     zl: u32,
     junction: u32,
     junction_number: u32,
-    request_status: u32
+    request_status: u32,
 }
 
 #[derive(Deserialize)]
 struct ValidR09_16Telegram {
     input: Vec<u8>,
-    output: TelegramFrame
+    output: TelegramFrame,
 }
 
 // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -39,7 +39,8 @@ fn test_decode_valid_r09_16_telegrams() {
     let server = "mockup".to_string();
     let decoder = Decoder::new(&config, &server);
     const FILE_STR: &'static str = include_str!("../../data/valid_r09_16_telegrams.json");
-    let parsed : Vec<ValidR09_16Telegram> = serde_json::from_str(&FILE_STR).expect("JSON was not well-formatted");
+    let parsed: Vec<ValidR09_16Telegram> =
+        serde_json::from_str(&FILE_STR).expect("JSON was not well-formatted");
 
     for (i, item) in parsed.iter().enumerate() {
         let telegram = &item.output;
@@ -49,14 +50,14 @@ fn test_decode_valid_r09_16_telegrams() {
             lat: 0.0,
             lon: 0.0,
             station_id: 0,
-            line: telegram.ln,
-            destination_number: telegram.zn,
+            line: format!("{:0>3}", telegram.ln.to_string()),
+            destination_number: format!("{:0>3}", telegram.zn.to_string()),
             priority: telegram.pr,
             sign_of_deviation: telegram.zv,
             value_of_deviation: telegram.zw,
             reporting_point: telegram.mp,
             request_for_priority: telegram.ha,
-            run_number: telegram.kn,
+            run_number: format!("{:0>2}", telegram.kn.to_string()),
             reserve: telegram.r,
             train_length: telegram.zl,
             junction: telegram.junction,
@@ -65,7 +66,7 @@ fn test_decode_valid_r09_16_telegrams() {
         };
 
         let received_telegram = decoder.full_decodation(&item.input.as_ref());
-        
+
         assert_eq!(received_telegram[0], expected_telegram);
 
         println!("{}", received_telegram[0]);
