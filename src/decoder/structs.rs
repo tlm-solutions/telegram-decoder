6c12b1ab3ea2d80d63454d13a92fe15a93ba9973
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2022-09-30 11:56:02 +0200

    add R09.14 parser

--- src/decoder/structs.rs
+++ src/decoder/structs.rs
@@ -26,6 +26,13 @@ impl BCD {
 }
 
 pub fn parse_r09_telegram(byte_array: &[u8]) -> Option<R09Telegram> {
+
+    let telegram_type = match byte_array[1] & 0xf {
+        4 => R09Types::R14,
+        6 => R09Types::R16,
+        _ => return None,
+    };
+
     let reporting_point: u32 = (((byte_array[2] >> 4) as u32) << 12u8) as u32
         | (((byte_array[2] & 0x0f) as u32) << 8) as u32
         | ((byte_array[3] >> 4) << 4) as u32
@@ -40,10 +47,17 @@ pub fn parse_r09_telegram(byte_array: &[u8]) -> Option<R09Telegram> {
         None => return None,
     };
     let destination =
-        match BCD::parse(&[byte_array[7] >> 4, byte_array[7] & 0xf, byte_array[8] >> 4]) {
-            Some(x) => x.0,
-            None => return None,
+        match telegram_type {
+            R09Types::R16 => match BCD::parse(&[byte_array[7] >> 4, byte_array[7] & 0xf, byte_array[8] >> 4]) {
+                    Some(x) => Some(x.0),
+                    None => return None,
+                },
+            _ => None,
         };
+    let train_length = match telegram_type {
+        R09Types::R16 => Some(byte_array[8] & 0x7),
+        _ => None,
+    };
 
     //TODO: marenz for all variants
 
@@ -51,7 +65,7 @@ pub fn parse_r09_telegram(byte_array: &[u8]) -> Option<R09Telegram> {
     let value_of_deviation = ((byte_array[1] >> 4) & 0x7) as i32; //ZW Zahlen Wert
 
     Some(R09Telegram {
-        telegram_type: R09Types::R16,
+        telegram_type: telegram_type,
         delay: Some((sign_of_deviation * -2 + 1) * value_of_deviation),
         reporting_point: reporting_point, //MP Melde punkt
         junction: (reporting_point >> 2) / 10,
@@ -61,8 +75,8 @@ pub fn parse_r09_telegram(byte_array: &[u8]) -> Option<R09Telegram> {
         direction_request: Some(((byte_array[4] >> 4) & 0x3) as u8), // HA Anforderung Richtung
         line: Some(line),                        // LN Line Nummer
         run_number: Some(run),                   // KN Kurse Nummer
-        destination_number: Some(destination),   // ZN Zielnummer
-        train_length: Some(byte_array[8] & 0x7), // ZL length
+        destination_number: destination,         // ZN Zielnummer
+        train_length: train_length,              // ZL length
         vehicle_number: None,
         operator: None,
     })
